68652413d12929ae1068648597a40b483fec39e5
Arman Darmesh <Darmesh.Ar@altynbank.kz>
2023-11-09 17:14:35 +0600

    add: aliases git

---
+++ aliases.zsh
@@ -0,0 +1,28 @@
+# Shortcuts
+
+## git
+
+alias nah="git reset --hard && git clean -df"
+
+alias gs="git status"
+alias gco="git checkout"
+
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gll="gl master.."
+alias gllb="gll --boundary"
+
+alias gaa="git add -A"
+
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gca="git commit --amend --no-edit"
+
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gdc="git diff @~ @"
+
+alias grst="git reset"
+
+alias gsa="git stash apply"
+alias gsl="git stash list"
+alias gss="git stash save"
